40afec12f93a525052d2082b3cd2912cbd5e3d03
tharle <tharlejc@gmail.com>
2018-07-27 12:11:16 -0400

    examples de vuejs

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var growler = new Vue({
+    el: '#growler',
+    data:{
+        appName: 'Growler',
+        decorator:{
+            accentColor: 'accent-color',
+            headers: 'headers'
+        }
+    }
+
+});
